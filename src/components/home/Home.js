a12c8d5458e7240fcd3df7d0397266e2e9b4ae9f
unknown <mustafamjaber@yahoo.com>
2023-01-02 14:20:05 +0200

    add the components folder and nave bar component

---
+++ src/components/home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Nave from "./nave/Nave";
+
+const Home = () => {
+  return <Nave />;
+};
+
+export default Home;
---
+++ src/components/home/nave/Nave.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Nave = () => {
+  return (
+    <nav className="nav ">
+      <div className="nav-wrapper">
+        <div className="nav-logo">
+          <a className="nav-logo">logo</a>
+        </div>
+        <div className="nav-items">
+          <ul className="nav nav-item">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">Cart</li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nave;

710e5143fdf2e1a912ee1ed0ed9eb5657b28df7e
unknown <mustafamjaber@yahoo.com>
2023-01-10 11:29:49 +0200

    add the amount section

--- src/components/cart/Carts.js
+++ src/components/cart/Carts.js
@@ -1,13 +1,19 @@
 import "./carts.css";
 import Cart from "./cart-comp/Cart";
+import Amount from "./amounts/Amount";
 
 const Carts = () => {
   return (
     <div className="carts-container">
-      <h1 className="cart-name">Your cart</h1>
-      <Cart />
-      <Cart />
-      <Cart />
+      {/* <h1 className="cart-name">Your cart</h1> */}
+      <div className="carts-section">
+        <Cart />
+        <Cart />
+        <Cart />
+      </div>
+      <div className="amount-section">
+        <Amount />
+      </div>
     </div>
   );
 };
---
+++ src/components/cart/amounts/Amount.js
@@ -0,0 +1,24 @@
+import "./amount.css";
+
+const Amount = () => {
+  return (
+    <>
+      <div className="amount-sec soubtotal">
+        <h2>Subtotal</h2>
+        <p>300 $</p>
+      </div>
+      <div className="amount-sec tax">
+        <h2>Tax</h2>
+        <p>6.22 $</p>
+      </div>
+      <hr></hr>
+      <div className="amount-sec total">
+        <h2>Total</h2>
+        <p>293.78 $</p>
+      </div>
+      <button className="checkout-btn">Checkoout</button>
+    </>
+  );
+};
+
+export default Amount;
